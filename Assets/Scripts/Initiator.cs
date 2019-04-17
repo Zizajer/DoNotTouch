d2576c9200d4feeef56f2d3cb34da384a1d3446c
Piotr <zizi5673@gmail.com>
2019-04-17 13:17:40 +0200

    Added creator object, which creates obstacles.

---
+++ Assets/Scripts/Initiator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initiator : MonoBehaviour
+{
+    public GameObject[] obstacles;
+    public Transform ObstacleParent;
+    Transform transform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        SpawnObstacle();
+    }
+
+    private void SpawnObstacle()
+    {
+            Instantiate(obstacles[0], transform.position, new Quaternion(0, 0, 0, 0), ObstacleParent);
+            Invoke("SpawnObstacle", 5f);
+    }
+}
